162c6065042828bd9580c4fc60fcc61a5ec926b2
rironib <dev.ronib@gmail.com>
2024-07-06 15:56:05 +0600

    Functionality added

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -1,7 +1,18 @@
 import {Helmet} from "react-helmet-async";
 import contact from '/mail.png';
+import {toast} from "react-toastify";
 
 const Contact = () => {
+    const handleOnSubmit = (e) => {
+        e.preventDefault();
+        const form = e.target;
+        const name = form.name.value;
+        const email = form.email.value;
+        const message = form.message.value;
+        toast.success('Successfully send!');
+        form.reset();
+    }
+
     return (
         <>
             <Helmet>
@@ -13,33 +24,33 @@ const Contact = () => {
                     <div className='dark:text-gray-400'>Contact with me</div>
                 </div>
                 <div className='grid lg:grid-cols-5 items-center gap-12 bg-slate-100 dark:bg-slate-800 py-12 px-6 lg:px-24 rounded-xl'>
-                    <div className='lg:col-span-2'>
+                    <div className='lg:col-span-2 mx-auto'>
                         <img src={contact} alt='Contact'/>
                     </div>
-                    <div className='lg:col-span-3'>
+                    <form onSubmit={(e) => handleOnSubmit(e)} className='lg:col-span-3'>
                         <div className='grid gap-6'>
                             <div className='w-full space-y-2'>
                                 <div className='font-medium text-lg'>Name</div>
-                                <input className='w-full px-6 py-3 border bg-white dark:bg-slate-900 dark:text-white rounded-lg outline-none'
-                                       placeholder='Enter Name'></input>
+                                <input name='name' type='text' className='w-full px-6 py-4 bg-white dark:bg-slate-900 dark:text-white rounded-lg outline-none'
+                                       placeholder='Enter Name' required></input>
                             </div>
                             <div className='w-full space-y-2'>
                                 <div className='font-medium text-lg'>Email</div>
-                                <input
-                                    className='w-full px-6 py-3 border bg-white dark:bg-slate-900 dark:text-white rounded-lg outline-none'
-                                    placeholder='Enter Email'></input>
+                                <input name='email' type='email'
+                                    className='w-full px-6 py-4 bg-white dark:bg-slate-900 dark:text-white rounded-lg outline-none'
+                                    placeholder='Enter Email' required></input>
                             </div>
                             <div className='w-full space-y-2'>
                                 <div className='font-medium text-lg'>Message</div>
-                                <textarea rows='5'
-                                    className='w-full px-6 py-3 border bg-white dark:bg-slate-900 dark:text-white rounded-lg outline-none'
-                                    placeholder='Write Your Message'></textarea>
+                                <textarea name='message' rows='5'
+                                    className='w-full px-6 py-4 bg-white dark:bg-slate-900 dark:text-white rounded-lg outline-none'
+                                    placeholder='Write Your Message' required></textarea>
                             </div>
-                            <button className='w-full bg-violet-600 text-white py-3 font-medium text-lg rounded-lg'>
+                            <button className='w-full bg-violet-600 text-white py-3 font-medium text-lg rounded-lg outline-none'>
                                 Send
                             </button>
                         </div>
-                    </div>
+                    </form>
                 </div>
             </main>
         </>
